b8ac31ba04f0dc7dc77a31bec89d7b56ca060e1e
Dan Loewenherz <dloewenherz@gmail.com>
2012-04-13 13:01:19 -0700

    first commit

---
+++ config.rb
@@ -0,0 +1,15 @@
+# Require any additional compass plugins here.
+
+# Set this to the root of your project when deployed:
+http_path = "/"
+css_dir = "css"
+sass_dir = "css"
+images_dir = "img"
+javascripts_dir = "js"
+line_comments = false
+
+# You can select your preferred output style here (can be overridden via the command line):
+# output_style = :expanded or :nested or :compact or :compressed
+
+# To enable relative paths to assets via compass helper functions. Uncomment:
+relative_assets = true
