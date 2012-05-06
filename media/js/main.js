95a3858a8cf792cfa1a1cc4b655962f0b7bc81b5
Dan Loewenherz <dloewenherz@gmail.com>
2012-05-06 11:59:02 -0700

    update domains.html

--- media/js/main.js
+++ media/js/main.js
@@ -1,13 +1,5 @@
 window.onload = function() {
     d = window.location.hash.substring(1);
-    if (d.length > 1) {
-        normal_inquiry.className = "hidden";
-        domain_inquiry.className = "";
-        email.setAttribute('href', 'mailto:domains@elmcitylabs.com?subject=I\'m%20interested%20in%20' + d);
-    }
-    else {
-        normal_inquiry.className = "";
-        domain_inquiry.className = "hidden";
-    }
+    email.setAttribute('href', 'mailto:domains@elmcitylabs.com?subject=I\'m%20interested%20in%20' + d);
 }
 
