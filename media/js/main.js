b79bdb9dcffcbe19808c1901d8793d9dcc55081d
Dan Loewenherz <dloewenherz@gmail.com>
2012-05-06 12:50:29 -0700

    change way to extract hostname

--- media/js/main.js
+++ media/js/main.js
@@ -1,5 +1,5 @@
 window.onload = function() {
-    d = window.location.hash.substring(1);
+    d = window.location.pathname.split('/')[2];
     email.setAttribute('href', 'mailto:domains@elmcitylabs.com?subject=I\'m%20interested%20in%20' + d);
 }
 
